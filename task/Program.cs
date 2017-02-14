c1b5791f4a5adab0bacccf87ee663db7a51506a1
koardu <arddy.korhonen@gmail.com>
2017-02-14 15:03:30 +0200

    Made new task

---
+++ task/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Type number: ");
+            int iNro = int.Parse(Console.ReadLine());
+
+
+            if (iNro < 0)
+            {
+                Console.WriteLine("You type number {0} and it is negative", iNro);
+            }
+
+            else if (iNro == 0)
+            {
+                Console.WriteLine("You type number {0} and it is zero", iNro);
+            }
+
+            else (iNro > 0)
+            {
+                Console.WriteLine("You type number {0} and it is positive", iNro);
+            }
+                Console.ReadKey();
+        }
+    }
+}
